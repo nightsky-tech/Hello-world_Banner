25bb28e9c334944d18401d19d3f6c37c1188bc29
nightsky-tech <astrokrithin@gmail.com>
2026-03-18 11:13:11 +0530

    Fixed HelloApp: displaying Hello World and command-line argument

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -3,10 +3,9 @@ public class HelloApp {
     public static void main(String[] args) {
 
         if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
+            System.out.println("Hello, " + args[0] + "!");
         } else {
-           System.out.println("Hello, User!");
+            System.out.println("Hello, World!");
         }
 
     }
