c48b9aab61232b889833adcb0ac8642133f6fff4
nightsky-tech <astrokrithin@gmail.com>
2026-03-26 09:32:37 +0530

    Added UC7: used String.join() for multiple names

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,12 +1,15 @@
 public class HelloApp {
     public static void main(String[] args) {
 
+        // Check for no arguments
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            for (String name : args) {   // enhanced for loop
-                System.out.println("Hello, " + name + "!");
-            }
+            // Join names with ", " delimiter
+            String names = String.join(", ", args);
+
+            // Print final greeting
+            System.out.println("Hello, " + names + "!");
         }
     }
 }
