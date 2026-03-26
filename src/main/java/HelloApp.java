1a4224a50330af7ffe310de7f789f63e186b1673
nightsky-tech <astrokrithin@gmail.com>
2026-03-26 09:52:08 +0530

    Fixed UC6: implemented substring method correctly

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,12 +1,24 @@
 public class HelloApp {
     public static void main(String[] args) {
 
+        // Check if no arguments are provided
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            for (String name : args) {   // enhanced for loop
-                System.out.println("Hello, " + name + "!");
+            StringBuilder nameBuilder = new StringBuilder();
+
+            // Append all names with ", "
+            for (String name : args) {
+                nameBuilder.append(name).append(", ");
             }
+
+            // Remove the last ", "
+            if (nameBuilder.length() > 0) {
+                nameBuilder.setLength(nameBuilder.length() - 2);
+            }
+
+            // Print final output
+            System.out.println("Hello, " + nameBuilder.toString() + "!");
         }
     }
 }
